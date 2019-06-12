7d1ac607e945b2a99da278af4566e3b3123c96cc
dengxinwei <dengxinwei@yy.com>
2019-06-12 18:36:04 +0800

    LRU

---
+++ src/main/java/com/dxw/cache/LRU/LinkedHashMapLRU.java
@@ -0,0 +1,74 @@
+package com.dxw.cache.LRU;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author ：dxw
+ * @date ：Created in 2019/6/12 16:25
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+public class LinkedHashMapLRU<K,V> {
+
+    private final int MAX_CACHE_SIZE;
+    private final float DEFAULT_LOAD_FACTORY = 0.75f;
+
+    private LinkedHashMap<K, V> map;
+
+    public LinkedHashMapLRU(int cacheSize) {
+        MAX_CACHE_SIZE = cacheSize;
+        int capacity = (int)Math.ceil(MAX_CACHE_SIZE / DEFAULT_LOAD_FACTORY) + 1;
+        /*
+         * 第三个参数设置为true，代表linkedlist按访问顺序排序，可作为LRU缓存
+         * 第三个参数设置为false，代表按插入顺序排序，可作为FIFO缓存
+         */
+        map = new LinkedHashMap<K, V>(capacity, DEFAULT_LOAD_FACTORY, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > MAX_CACHE_SIZE;
+            }
+        };
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    public synchronized void remove(K key) {
+        map.remove(key);
+    }
+
+    public synchronized Set<Map.Entry<K, V>> getAll() {
+        return map.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            stringBuilder.append(String.format("%s: %s  ", entry.getKey(), entry.getValue()));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedHashMapLRU<Integer, Integer> lru1 = new LinkedHashMapLRU(5);
+        lru1.put(1, 1);
+        lru1.put(2, 2);
+        lru1.put(3, 3);
+        System.out.println(lru1);
+        lru1.get(1);
+        System.out.println(lru1);
+        lru1.put(4, 4);
+        lru1.put(5, 5);
+        lru1.put(6, 6);
+        System.out.println(lru1);
+    }
+}
---
+++ src/main/java/com/dxw/cache/test.java
@@ -0,0 +1,11 @@
+package com.dxw.cache;
+
+/**
+ * @author ：dxw
+ * @date ：Created in 2019/6/12 16:24
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+public class test {
+}
